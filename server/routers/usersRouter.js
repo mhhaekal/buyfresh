133554a919e42d4791e2f2a657857841f7ec1b2f
mhhaekal <70019961+mhhaekal@users.noreply.github.com>
2023-10-11 21:55:39 +0700

    Delete server directory

--- server/routers/usersRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-
-// Import All Controller
-
-const { usersController } = require("../controllers");
-Router.post("/login", usersController.login);
-module.exports = Router;
